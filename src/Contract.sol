64e2a710b577afd0f1cac037e908b535f03e5128
raveeshcool <raveesh_g@me.iitr.ac.in>
2025-01-17 14:15:54 +0530

    smol tests

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,20 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract Contract { }
+contract Counter {
+    uint public count ;
+
+    constructor(uint _count){
+        count = _count;
+    }
+
+    function increment() public {
+        count++;
+    }
+    
+    function decrement() public {
+        count--;
+    }
+
+
+}
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -1,23 +1,32 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
-
+import "src/Contract.sol"; 
 import "forge-std/Test.sol";
 
-import "src/Contract.sol";
 
-contract TestContract is Test {
-    Contract c;
+contract TestCounter is Test {
+    Counter c;
 
-    function setUp() public {
-        c = new Contract();
+    function setUp() public {//called before all the tests run
+        c = new Counter(100);
+    }
+    function testIncrement() public{
+        assertEq(c.count(),100);
+        c.increment();
+        c.increment();
+        assertEq(c.count(),102);
     }
 
-    function testBar() public {
-        assertEq(uint256(1), uint256(1), "ok");
+    function testDecrement() public{
+        c.decrement();
+        c.decrement();
+        assertEq(c.count(),98,"ok");
     }
+// Foundry automatically expects failure in any test prefixed with testFail
+    function testFailDecrement()public {
+        for (uint256 index = 0; index < 101; index++) {
+            c.decrement();
+        }
 
-    function testFoo(uint256 x) public {
-        vm.assume(x < type(uint128).max);
-        assertEq(x + x, x * 2);
     }
-}
+}  
